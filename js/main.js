16283e6f561244348c7a22cea2743e00b45fa507
LLetzel <lucasdiasletzel@gmail.com>
2023-12-26 00:01:16 -0300

    aula 6 stop

--- js/main.js
+++ js/main.js
@@ -1,6 +1,22 @@
+/*
 document.getElementById("texto").innerHTML="meu primeiro texto <b>JS</b>!";
 //<b> </b> deixar conteúdo em negrito
 
 alert(5+5)
 
 console.log("oi isso é um console.log")
+
+let a,b,c;
+a = 5;
+b = 6;
+c = 7;
+
+function minhafuncao(){
+    document.write("<u>Oi escrito com document.write</u>");
+}
+*/
+
+var a = 1
+var b = 5
+var c = a + b
+console.log(c);
