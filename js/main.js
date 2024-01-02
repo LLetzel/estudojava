fddf9373b5cc8cdb67a609a554b1b131860b0c1a
LLetzel <lucasdiasletzel@gmail.com>
2024-01-02 02:58:55 -0300

    aula 7

--- js/main.js
+++ js/main.js
@@ -16,7 +16,32 @@ function minhafuncao(){
 }
 */
 
-var a = 1
-var b = 5
-var c = a + b
-console.log(c);
+// var a = 1
+// var b = 5
+// var c = a + b
+// console.log(c);
+
+/*
+No javascrip temos 3 palavras-chaves para declarar variáveis:
+-> var
+-> let
+-> const
+*/
+/*
+// DECLARAÇÃO DE VARIÁVEIS
+var nome, sobrenome, nomeCompleto;
+
+//ATRIBUIÇÃO DE VALORES
+nome = "Dimitri";
+sobrenome = "Teixeira";
+idade = 30;
+nomeCompleto = nome + " " + sobrenome;
+
+pessoa = idade + nome
+
+document.getElementById("texto").innerHTML = pessoa;
+*/
+var idade = 18;
+var eleitor = (idade<18) ? "Não, não eleitor": "Sim, Eleitor"
+
+alert("a resposta é: " + eleitor + " a idade dele é de " + idade); 
